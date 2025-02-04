8939771eed3f7f11400177a373451d296ba6e2c9
Dedift <staszhvansky@gmail.com>
2025-02-04 17:04:52 +0300

    add tests and refactor bugs

--- src/main/ru/aston/zhvansky_sv/main_task/domain/Client/Order.java
+++ src/main/ru/aston/zhvansky_sv/main_task/domain/Client/Order.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class Order {
 
     public static final double ZERO = 0.0;
-    private ArrayList<Medicine> medicines;
+    private ArrayList<Medicine> medicines =  new ArrayList<>();
     private User user;
 
     public Order(ArrayList<Medicine> medicines, User user) {
@@ -40,11 +40,11 @@ public class Order {
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Order order)) return false;
-        return Objects.equals(medicines, order.medicines) && Objects.equals(user, order.user);
+        return Objects.equals(medicines, order.medicines);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(medicines, user);
+        return Objects.hashCode(medicines);
     }
 }
---
+++ src/test/java/domain/Utils/OrdersHistoryTest.java
@@ -0,0 +1,64 @@
+package domain.Utils;
+
+import domain.Client.*;
+import domain.Medicine.Cream;
+import domain.Medicine.Drops;
+import domain.Medicine.Pill;
+import domain.Medicine.Powder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersHistoryTest {
+
+    Order firstOrder;
+    Order secondOrder;
+
+
+    @BeforeEach
+    void setUp() {
+        Pill pill = new Pill("Aspirin", 10.80, false, "acetylsalicylic acid", 20);
+        Cream cream = new Cream("Diclofenac", 5.23, true, "diclofenac", 100);
+        Powder powder = new Powder("TheraFlu", 20.4, false, "paracetamol", 10);
+        Drops drops = new Drops("Artelac Splash", 31.43, false, "sodium hyaluronate", 10);
+
+        User firstUser = new User(null, null, "Lu", "Zhvanskaya", 29, null, Gender.FEMALE, 300.2);
+        User secondUser = new User(null, null, "Alina", "Plush", 19, PersonType.DEFAULT, Gender.FEMALE, 300.2);
+        firstUser.setRecipes(new ArrayList<>() {{
+            add(new Recipe(firstUser, cream));
+        }});
+        secondUser.setRecipes(new ArrayList<>() {{
+            add(new Recipe(secondUser, cream));
+        }});
+
+        this.firstOrder = firstUser.createOrder(new ArrayList<>() {{
+            add(pill);
+            add(cream);
+            add(powder);
+            add(drops);
+        }});
+
+        this.secondOrder = secondUser.createOrder(new ArrayList<>() {{
+            add(pill);
+            add(cream);
+            add(powder);
+            add(drops);
+        }});
+    }
+
+    @Test
+    void getSortedOrders() {
+
+        ArrayList<Order> ordersForSort = new ArrayList<>() {{
+            add(firstOrder);
+            add(secondOrder);
+        }};
+        ordersForSort.sort(Comparator.comparing(o -> o.getUser().getSurName()));
+        Assertions.assertEquals(secondOrder.getUser().getSurName(), ordersForSort.get(0).getUser().getSurName());
+    }
+}
